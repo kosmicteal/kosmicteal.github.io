9dcb97b6848614554ab8d29fa9463026648b5fcd
KosmicTeal <69432458+kosmicteal@users.noreply.github.com>
2025-03-02 19:19:25 +0100

    Updates

--- assets/u-CTLnRSN8.js
+++
@@ -1,9 +0,0 @@
-import{p as _}from"./r-CxA_uJUv.js";var s={exports:{}},m={};/**
- * @license React
- * use-sync-external-store-with-selector.production.js
- *
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */var h;function D(){if(h)return m;h=1;var f=_();function E(r,u){return r===u&&(r!==0||1/r===1/u)||r!==r&&u!==u}var V=typeof Object.is=="function"?Object.is:E,j=f.useSyncExternalStore,p=f.useRef,w=f.useEffect,z=f.useMemo,M=f.useDebugValue;return m.useSyncExternalStoreWithSelector=function(r,u,d,v,o){var t=p(null);if(t.current===null){var n={hasValue:!1,value:null};t.current=n}else n=t.current;t=z(function(){function S(e){if(!R){if(R=!0,c=e,e=v(e),o!==void 0&&n.hasValue){var i=n.value;if(o(i,e))return l=i}return l=e}if(i=l,V(c,e))return i;var W=v(e);return o!==void 0&&o(i,W)?(c=e,i):(c=e,l=W)}var R=!1,c,l,b=d===void 0?null:d;return[function(){return S(u())},b===null?void 0:function(){return S(b())}]},[u,d,v,o]);var a=j(r,t[0],t[1]);return w(function(){n.hasValue=!0,n.value=a},[a]),M(a),a},m}var y;function O(){return y||(y=1,s.exports=D()),s.exports}var q=O();export{q as w};
